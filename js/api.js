a44c2fc493b6d13e8ee5293ab8ff9b14c7f5a18c
GolovinaElia <eliagolovina@ukr.net>
2021-07-23 19:16:18 +0300

    Добавляет файлы

--- js/api.js
+++ js/api.js
@@ -1,18 +1,73 @@
-const BASE_URL = "https://restcountries.eu/rest/v2"
+// let form = document.getElementById("form")
+const form = document.querySelector(".form")
 
-export default class CountriesApiService {
-  constructor() {
-    this.searchQuery = ""
+form.addEventListener("submit", async function (event) {
+  event.preventDefault()
+  let response = await fetch("https://asiansy.com/test/test.php", {
+    method: "POST",
+    body: new FormData(form),
+  })
+  if (response.ok) {
+    let text = await response.text()
+    alert(text)
+  } else {
+    alert(response.status)
   }
-  fetchCountries(searchQuery) {
-    return fetch(`${BASE_URL}/name/${this.searchQuery}`).then((response) => {
-      return response.json()
-    })
-  }
-  get query() {
-    return this.searchQuery
-  }
-  set query(newQuery) {
-    this.searchQuery = newQuery
-  }
-}
+})
+
+// form.onsubmit = async (event) => {
+//   event.preventDefault()
+//   let response = await fetch("https://asiansy.com/test/test.php", {
+//     method: "POST",
+//     body: new FormData(form),
+//   })
+//   if (response.ok) {
+//     let text = await response.text()
+//     alert(text)
+//   } else {
+//     alert(response.status)
+//   }
+// }
+
+// form.addEventListener("submit", function (event) {
+//   event.preventDefault()
+// let name = this.querySelector('[name="name"]').value
+// let phone = this.querySelector('[name="phone"]').value
+
+// let formData = new FormData()
+// formData.append("name", name)
+// formData.append("phone", phone)
+// alert(formData.get("name"))
+
+// let promise = fetch("https://asiansy.com/test/test.php", {
+//   method: "POST",
+//   // body: new FormData(this),
+//   body: formData,
+//   mode: "no-cors",
+// })
+
+// promise
+//   .then((response) => {
+//     response.text()
+//     return console.log(response)
+//   })
+//   .then((text) => {
+//     console.log(text)
+//   })
+//   .catch((error) => console.log(error))
+
+// })
+// form.onsubmit = async (event) => {
+//   event.preventDefault()
+//   let response = await fetch("https://asiansy.com/test/test.php", {
+//     method: "POST",
+//     body: new FormData(form),
+//   })
+//   if (response.ok) {
+//     let result = await response.text()
+//     alert(texe)
+//   } else {
+//     alert(response.status)
+//   }
+// let result = await response.json()
+// alert(result.message)
--- js/timer.js
+++ js/timer.js
@@ -2,18 +2,10 @@ class Timer {
   constructor({ selector, targetDate }) {
     this.targetDate = new Date(targetDate)
     ;(this.intervalId = null),
-      (this.days = document.querySelector(
-        `${selector} .value[data-value="days"]`
-      ))
-    this.hours = document.querySelector(
-      `${selector} .value[data-value="hours"]`
-    )
-    this.minutes = document.querySelector(
-      `${selector} .value[data-value="mins"]`
-    )
-    this.seconds = document.querySelector(
-      `${selector} .value[data-value="secs"]`
-    )
+      (this.days = document.querySelector(`${selector} .value[data-value="days"]`))
+    this.hours = document.querySelector(`${selector} .value[data-value="hours"]`)
+    this.minutes = document.querySelector(`${selector} .value[data-value="mins"]`)
+    this.seconds = document.querySelector(`${selector} .value[data-value="secs"]`)
     this.timer()
   }
 
@@ -30,12 +22,8 @@ class Timer {
         this.hours.innerHTML = this.pad(
           Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
         )
-        this.minutes.innerHTML = this.pad(
-          Math.floor((time % (1000 * 60 * 60)) / (1000 * 60))
-        )
-        this.seconds.innerHTML = this.pad(
-          Math.floor((time % (1000 * 60)) / 1000)
-        )
+        this.minutes.innerHTML = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)))
+        this.seconds.innerHTML = this.pad(Math.floor((time % (1000 * 60)) / 1000))
       } else {
         clearInterval(this.intervalId)
       }
